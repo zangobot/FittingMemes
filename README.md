acf614277d7d42feefebb72c9311a13a1e32c871
Luca Demetrio <luca.demetrio@outlook.it>
2018-06-24 19:00:50 +0200

    Added dumb_classifier 2
    ... which is a Gaussian SVM

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ I've downloaded a bunch of memes from a popular site (link in code) and try to f
 * **SPOILER 0x1**: the problem is super easy, beacuse all memes are very representative for themself AND basically all the images are the same except the caption;
 * **SPOILER 0x2**: I WANT TO TRY SIMPLE THINGS.
 
-Having said that, you may want to have a look at the code...
+Having said that, you may want to have a look at the code... or the [notebook](https://github.com/zangobot/FittingMemes/blob/master/Why%3F.ipynb) I'm creating!
--- svc_dumb_classifier.py
+++ svc_dumb_classifier.py
@@ -1,21 +1,29 @@
 from data_generator import  train_test_split, generate_mapping, create_dataset
 from sklearn.cluster import KMeans
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 import numpy as np
 import matplotlib.pyplot as plt
 
 X_train, y_train, X_tst, y_tst = train_test_split(test_size=0.7, subsample=1, persistance=False)
 print("The shape of the problem is {}".format(X_train.shape))
 
-forest = RandomForestClassifier(n_estimators=15)
-forest.fit(X_train, y_train)
+grid = {
+    'gamma' : np.logspace(-1,1,10),
+    'C' : np.logspace(-1,1,10)
+}
 
-y_pred = forest.predict(X_tst)
+gscv = GridSearchCV( SVC(kernel='rbf') , param_grid=grid, cv=StratifiedShuffleSplit(n_splits=10,test_size=0.25))
+gscv.fit(X_train, y_train)
+
+clf = gscv.best_estimator_
+
+y_pred = clf.predict(X_tst)
 
 error =  100*np.sum( y_pred != y_tst ) / len(y_tst)
+print('This time the error is {0:.2f}'.format(error))
 
-print('And the error is ONLY {0:.2f} %!'.format(error))
-result = np.sort(forest.feature_importances_)
+result = np.sort(clf.coef_)
 result = result[::-1]
 plt.semilogy(result[:300])
 plt.title('Importance of features')
